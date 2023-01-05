2a6c4485318f642a49ff6e67cdb9b8db7b34f430
rundekugel <rundekugel@gmx.de>
2023-01-05 18:29:10 +0100

    get input from date and time

--- input/input.cob
+++ input/input.cob
@@ -5,12 +5,16 @@ environment division.
 data division.
 working-storage section.
 01 var1   PIC X(22).
+01 date1   PIC x(10).
+01 tim1   pic x(6).
 procedure division.
 display "Hello!".
 *> just a comment
 *> comments must have * and >
 display "type something:"
 accept var1.
-display "you typed: " var1.
+accept date1 from date.
+accept tim1 from time.
+display "you typed: " var1 ". date: " date1 "-" tim1.
 display "--- done. ---"
 goback.
